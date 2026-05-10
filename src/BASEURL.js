a7ef2f51f8e81640368cdc527d0f45bb09c1fcfc
Ramesh-Chaudhary <rameshchaudhary2254@gmail.com>
2026-05-10 09:08:49 +0530

    Fixed backend URL

--- src/BASEURL.js
+++ src/BASEURL.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "https://yugantar.onrender.com";
 // const BASE_URL = "http://localhost:5000";
-const BASE_URL = "https://yugantar.onrender.com";
+const BASE_URL = "https://yu-backend-twjk.onrender.com";
 export default BASE_URL;
